e8229a740c4f06e4b798a21c68ae01f0c1ae2eda
himanshu789-hub <khemanihimi78@gmail.com>
2021-03-31 16:16:08 +0530

    Invoices UI Started

--- src/Components/CaretSize/CaretSize.tsx
+++ src/Components/CaretSize/CaretSize.tsx
@@ -3,12 +3,12 @@ import { provideValidNumber } from 'Utilities/Utilities';
 interface ICaretSizeProps {
 	Size: number;
 	handleInput: Function;
-	Limit?:number;
+	Limit?: number;
 }
 const CaretSize = function CaretSize(props: ICaretSizeProps) {
 	const [caret, setCart] = useState<number>(0);
 	const [pieces, setPieces] = useState<number>(0);
-	const [quantity,setQuantity] = useState<number>(0);
+	const [quantity, setQuantity] = useState<number>(0);
 	const { Size, handleInput } = props;
 	const Max_Caret_Allow = 100;
 	const Max_Pieces_Allow = props.Size;
@@ -41,21 +41,27 @@ const CaretSize = function CaretSize(props: ICaretSizeProps) {
 		}
 	};
 	return (
-		<div className={`form-group p-1 ${!props.Size ? 'disabled' : ''} ${!props.Limit?'':quantity>props.Limit?'border border-danger rounded is-invalid':''}`}>
-			<label htmlFor=''>Quantity</label>
-			<div className='d-flex justify-content-around'>
-				<div className={`form-group `}>
-					<input className='form-control' value={caret} onChange={handleCaretChange} />
-					<small className='form-text text-muted'>Caret</small>
+		<div className={!quantity ? 'border border-danger is-invalid rounded' : ''}>
+			<div
+				className={`form-group p-1 ${!props.Size ? 'disabled' : ''} ${
+					!props.Limit ? '' : quantity > props.Limit ? 'border border-danger rounded is-invalid' : ''
+				}`}>
+				<label htmlFor=''>Quantity</label>
+				<div className='d-flex justify-content-around'>
+					<div className={`form-group `}>
+						<input className='form-control' value={caret} onChange={handleCaretChange} />
+						<small className='form-text text-muted'>Caret</small>
+					</div>
+					<label className='pl-2 pr-2 font-weight-bold'>:</label>
+					<div className={`form-group ${pieces > props.Size ? 'is-invalid' : ''}`}>
+						<input className='form-control' value={pieces} onChange={handlePiecesChange} />
+						<small className='form-text text-muted'>Pieces</small>
+						<small className='invalid-feedback'>Atmost {props.Size}</small>
+					</div>
 				</div>
-				<label className='pl-2 pr-2 font-weight-bold'>:</label>
-				<div className={`form-group ${pieces>props.Size?'is-invalid':''}`}>
-					<input className='form-control' value={pieces} onChange={handlePiecesChange} />
-					<small className='form-text text-muted'>Pieces</small>
-					<small className="invalid-feedback">Atmost {props.Size}</small>
-				</div>
+				<div className='invalid-feedback pl-1'>Quantity Exceed</div>
 			</div>
-			<div className="invalid-feedback pl-1">Quantity Exceed</div>
+			<div className='invalid-feedback pl-1'>Cannot Be Zero</div>
 		</div>
 	);
 };
---
+++ src/Containers/Invoices/Invoices.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type InvoicesProps = {};
+type InvoicesState = {};
+export default class Invoices extends React.Component<InvoicesProps, InvoicesState> {
+	constructor(props: InvoicesProps) {
+		super(props);
+	}
+}
